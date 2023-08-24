d3a340d5d4e23044b5b6ce47289c056c286534fc
Batuhan Yalçıntürk <batuhanyalcinturk@gmail.com>
2023-08-24 13:20:42 +0300

    TechCareer Final Project

---
+++ src/main/java/com/graysan/TechcareerFinalProjectApplication.java
@@ -0,0 +1,13 @@
+package com.graysan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechcareerFinalProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechcareerFinalProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/graysan/TechcareerFinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.graysan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechcareerFinalProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
