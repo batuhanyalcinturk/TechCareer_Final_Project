e1ca9c6fa2fafb1ae7c6bca734ed14a81250d3e7
Batuhan Yalçıntürk <batuhanyalcinturk@gmail.com>
2023-08-25 00:21:52 +0300

    ToDoItem and User Repository added and fixing

--- src/main/java/com/graysan/data/repository/ToDoItemRepository.java
+++ src/main/java/com/graysan/data/repository/ToDoItemRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface ToDoItemRepository extends JpaRepository {
+public interface ToDoItemRepository extends JpaRepository<ToDoItem, Long> {
 
     int countAllByCompleted(boolean completed);
 
---
+++ src/main/java/com/graysan/data/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.graysan.data.repository;
+
+import com.graysan.data.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUserName(String username);
+}
