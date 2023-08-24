47e4f99b3c5d58169c8e8c7c53972f00ba15a2c9
Batuhan Yalçıntürk <batuhanyalcinturk@gmail.com>
2023-08-25 00:46:32 +0300

    User Model fixing

--- src/main/java/com/graysan/data/entity/User.java
+++ src/main/java/com/graysan/data/entity/User.java
@@ -1,23 +1,39 @@
 package com.graysan.data.entity;
 
 import jakarta.persistence.*;
-import lombok.ToString;
+import lombok.*;
+import lombok.extern.log4j.Log4j2;
 
 import java.util.List;
 
+@Data
+@Log4j2
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
 public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id", unique = true, nullable = false, insertable = true, updatable = false)
     private long userId;
 
-    private String fullName;
-
-    private String username;
+    private String userName;
 
     private String password;
 
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     @ToString.Exclude
     private List<ToDoItem> todos;
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userId=" + userId +
+                ", userName='" + userName + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+
 }
